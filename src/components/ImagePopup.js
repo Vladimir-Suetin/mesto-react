27ce45058a3ce68c26c92847c28aa1f893298851
Suetin_Vladimir <Suetin-vladimir@yandex.ru>
2023-02-01 19:59:27 +0300

    реализовть закрытие окна просмотра фото при нажатии на внешюю область

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -1,8 +1,14 @@
 import React from 'react';
 
 function ImagePopup({ card, onClose }) {
+  function handleOverlayClick(evt) {
+    if (evt.target === evt.currentTarget) {
+      onClose();
+    }
+  }
+
   return (
-    <div className={`popup popup_view_image ${card.link && 'popup_opened'}`}>
+    <div className={`popup popup_view_image ${card.link && 'popup_opened'}`} onMouseDown={handleOverlayClick}>
       <div className='popup__wrapper'>
         <button type='button' className='popup__close-icon' aria-label='закрыть форму' onClick={onClose}></button>
         <img src={card.link} alt={card.name} className='popup__image' />
